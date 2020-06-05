280ad33262880db22d2ac6a9afdd15730bd6bb20
Dzmitry Malyshau <dmalyshau@mozilla.com>
2020-06-05 16:45:19 -0400

    Add PBO upload tests

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,23 @@ unsafe fn link_program<C: glow::HasContext>(gl: &C, vs: &str, ps: &str) -> C::Pr
     program
 }
 
+fn report(name: &str, actual: [u8; 4], expected: [u8; 4]) {
+    print!("\t{}: ", name.color(Color::Blue));
+    if actual == expected {
+        println!("{}", "PASS".color(Color::Green));
+    } else {
+        println!("{} {:?}", "FAIL".color(Color::Red), actual);
+    }
+}
+
+/// Swizzle test.
+///
+/// Setting up the swizzle on a texture unit may corrupt the shader metadata,
+/// making the `textureSize` return garbage from the shader.
+///
+/// Affected: Intel 4000 GPU on macOS
 fn test_swizzle<C: glow::HasContext>(gl: &C, extensions: &[String]) {
-    println!("Test: {}", "swizzled texture unit".color(Color::Blue));
+    println!("Test: {}", "swizzle".color(Color::DarkBlue));
     print!("\tRelevant extensions:");
     for ext in extensions {
         if ext.contains("swizzle") {
@@ -48,7 +63,8 @@ fn test_swizzle<C: glow::HasContext>(gl: &C, extensions: &[String]) {
         }
     "#;
 
-    let texel = unsafe {
+    let mut texel = [0u8; 4];
+    unsafe {
         // initialize the texture data
         let size = 64;
         let texture = gl.create_texture().unwrap();
@@ -101,7 +117,6 @@ fn test_swizzle<C: glow::HasContext>(gl: &C, extensions: &[String]) {
         gl.draw_arrays(glow::TRIANGLE_STRIP, 0, 4);
 
         // read back a texel
-        let mut texel = [0u8; 4];
         gl.read_pixels(
             size / 2,
             size / 2,
@@ -111,15 +126,109 @@ fn test_swizzle<C: glow::HasContext>(gl: &C, extensions: &[String]) {
             glow::UNSIGNED_BYTE,
             &mut texel,
         );
-        texel
     };
 
-    const EXPECTED: [u8; 4] = [0xFF, 0xFF, 0, 0xFF];
-    if texel == EXPECTED {
-        println!("\t{}", "PASS".color(Color::Green));
-    } else {
-        println!("\t{} {:?}", "FAIL".color(Color::Red), texel);
+    report("textureSize", texel, [0xFF, 0xFF, 0, 0xFF]);
+}
+
+fn test_pbo_upload<C: glow::HasContext>(gl: &C) {
+    println!("Test: {}", "PBO uploads".color(Color::DarkBlue));
+    const FILL: [u8; 4] = [0, 0xFF, 0, 0xFF];
+    let size = (256, 16);
+    let mut texels = [[0u8; 4]; 3];
+    unsafe {
+        // initialize the texture
+        let texture = gl.create_texture().unwrap();
+        gl.bind_texture(glow::TEXTURE_2D_ARRAY, Some(texture));
+        gl.tex_storage_3d(glow::TEXTURE_2D_ARRAY, 1, glow::RGBA8, size.0, size.1, 2);
+        gl.tex_sub_image_3d_u8_slice(
+            glow::TEXTURE_2D_ARRAY,
+            0,
+            0,
+            0,
+            0,
+            size.0,
+            size.1,
+            2,
+            glow::RGBA,
+            glow::UNSIGNED_BYTE,
+            Some(&vec![0xFF; (size.0 as usize) * (size.1 as usize) * 4]),
+        );
+
+        // initialize a framebuffer for reading pixels back
+        let framebuf = gl.create_framebuffer().unwrap();
+        gl.bind_framebuffer(glow::FRAMEBUFFER, Some(framebuf));
+        gl.framebuffer_texture_layer(
+            glow::FRAMEBUFFER,
+            glow::COLOR_ATTACHMENT0,
+            Some(texture),
+            0,
+            0,
+        );
+        gl.read_pixels(0, 0, 1, 1, glow::RGBA, glow::UNSIGNED_BYTE, &mut texels[0]);
+
+        // create a pixel buffer with one line
+        let pixelbuf = gl.create_buffer().unwrap();
+        gl.bind_buffer(glow::PIXEL_UNPACK_BUFFER, Some(pixelbuf));
+        gl.buffer_data_u8_slice(
+            glow::PIXEL_UNPACK_BUFFER,
+            &(0..size.0)
+                .flat_map(|_| FILL.iter().cloned())
+                .collect::<Vec<_>>(),
+            glow::STATIC_DRAW,
+        );
+        let texel_offset = 16;
+        let pbo_offset = 128 * 4;
+        gl.tex_sub_image_3d_pixel_buffer_offset(
+            glow::TEXTURE_2D_ARRAY,
+            0,
+            texel_offset,
+            0,
+            0,
+            16,
+            1,
+            1,
+            glow::RGBA,
+            glow::UNSIGNED_BYTE,
+            pbo_offset,
+        );
+        gl.read_pixels(
+            texel_offset,
+            0,
+            1,
+            1,
+            glow::RGBA,
+            glow::UNSIGNED_BYTE,
+            &mut texels[1],
+        );
+
+        // upload to the other layer of the texture
+        gl.tex_sub_image_3d_pixel_buffer_offset(
+            glow::TEXTURE_2D_ARRAY,
+            0,
+            0,
+            0,
+            1,
+            16,
+            1,
+            1,
+            glow::RGBA,
+            glow::UNSIGNED_BYTE,
+            0,
+        );
+        gl.framebuffer_texture_layer(
+            glow::FRAMEBUFFER,
+            glow::COLOR_ATTACHMENT0,
+            Some(texture),
+            0,
+            1,
+        );
+        gl.read_pixels(0, 0, 1, 1, glow::RGBA, glow::UNSIGNED_BYTE, &mut texels[2]);
     }
+
+    assert_eq!(texels[0], [0xFF; 4]);
+    report("offset-128px", texels[1], FILL);
+    report("layer-1", texels[2], FILL);
 }
 
 fn main() {
@@ -161,6 +270,7 @@ fn main() {
     println!("Init with renderer: {}", renderer.color(Color::Violet));
 
     test_swizzle(&gl, &extensions);
+    test_pbo_upload(&gl);
 
     if has_khr_debug {
         let debug_messages = unsafe { gl.get_debug_message_log(10) };
