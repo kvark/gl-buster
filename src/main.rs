cec9520ee4df8cc642e96116f22d03d8d312a985
Dzmitry Malyshau <dmalyshau@mozilla.com>
2020-06-05 15:33:45 -0400

    Switch to surfman, move swizzling out into a function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,130 @@
-use glow::{Context, RenderLoop};
+use colorful::{Color, Colorful};
+
+unsafe fn link_program<C: glow::HasContext>(gl: &C, vs: &str, ps: &str) -> C::Program {
+    let program = gl.create_program().unwrap();
+    let shader_sources = [(glow::VERTEX_SHADER, vs), (glow::FRAGMENT_SHADER, ps)];
+    for (shader_type, shader_source) in shader_sources.iter() {
+        let shader = gl.create_shader(*shader_type).unwrap();
+        gl.shader_source(shader, shader_source);
+        gl.compile_shader(shader);
+        if !gl.get_shader_compile_status(shader) {
+            panic!(gl.get_shader_info_log(shader));
+        }
+        gl.attach_shader(program, shader);
+    }
+    gl.link_program(program);
+    if !gl.get_program_link_status(program) {
+        panic!(gl.get_program_info_log(program));
+    }
+    program
+}
+
+fn test_swizzle<C: glow::HasContext>(gl: &C, extensions: &[String]) {
+    println!("Test: {}", "swizzled texture unit".color(Color::Blue));
+    print!("\tRelevant extensions:");
+    for ext in extensions {
+        if ext.contains("swizzle") {
+            print!(" {}", ext.as_str().color(Color::Yellow));
+        }
+    }
+    println!("");
+
+    let vs_shader = r#"#version 330 core
+        uniform sampler2DArray tex;
+        out vec4 color;
+        void main() {
+            vec2 tc = vec2(ivec2(gl_VertexID/2, gl_VertexID%2));
+            vec2 tex_size = vec2(textureSize(tex, 0).xy);
+            color = vec4(tex_size/64.0, 0.0, 1.0);
+            gl_Position = vec4(2.0*tc - 1.0, 0.0, 1.0);
+        }
+    "#;
+    let fs_shader = r#"#version 330 core
+        precision mediump float;
+        in vec4 color;
+        out vec4 o_Color;
+        void main() {
+            o_Color = color;
+        }
+    "#;
+
+    let texel = unsafe {
+        // initialize the texture data
+        let size = 64;
+        let texture = gl.create_texture().unwrap();
+        gl.bind_texture(glow::TEXTURE_2D_ARRAY, Some(texture));
+        gl.tex_storage_3d(glow::TEXTURE_2D_ARRAY, 1, glow::RGBA8, size, size, 2);
+        gl.tex_parameter_i32(
+            glow::TEXTURE_2D_ARRAY,
+            glow::TEXTURE_SWIZZLE_R,
+            glow::BLUE as i32,
+        );
+        gl.tex_parameter_i32(
+            glow::TEXTURE_2D_ARRAY,
+            glow::TEXTURE_SWIZZLE_G,
+            glow::GREEN as i32,
+        );
+        gl.tex_parameter_i32(
+            glow::TEXTURE_2D_ARRAY,
+            glow::TEXTURE_SWIZZLE_B,
+            glow::RED as i32,
+        );
+        gl.tex_parameter_i32(
+            glow::TEXTURE_2D_ARRAY,
+            glow::TEXTURE_SWIZZLE_A,
+            glow::ALPHA as i32,
+        );
+
+        // link the program reads the texture size from the VS
+        let program = link_program(gl, vs_shader, fs_shader);
+
+        // prepare to draw
+        let draw_size = 256;
+        let vertex_array = gl.create_vertex_array().unwrap();
+        gl.bind_vertex_array(Some(vertex_array));
+        let renderbuf = gl.create_renderbuffer().unwrap();
+        gl.bind_renderbuffer(glow::RENDERBUFFER, Some(renderbuf));
+        gl.renderbuffer_storage(glow::RENDERBUFFER, glow::RGBA8, draw_size, draw_size);
+        let framebuf = gl.create_framebuffer().unwrap();
+        gl.bind_framebuffer(glow::FRAMEBUFFER, Some(framebuf));
+        gl.framebuffer_renderbuffer(
+            glow::FRAMEBUFFER,
+            glow::COLOR_ATTACHMENT0,
+            glow::RENDERBUFFER,
+            Some(renderbuf),
+        );
+
+        // draw a quad
+        gl.clear(glow::COLOR_BUFFER_BIT);
+        gl.viewport(0, 0, draw_size, draw_size);
+        gl.use_program(Some(program));
+        gl.draw_arrays(glow::TRIANGLE_STRIP, 0, 4);
+
+        // read back a texel
+        let mut texel = [0u8; 4];
+        gl.read_pixels(
+            size / 2,
+            size / 2,
+            1,
+            1,
+            glow::RGBA,
+            glow::UNSIGNED_BYTE,
+            &mut texel,
+        );
+        texel
+    };
+
+    const EXPECTED: [u8; 4] = [0xFF, 0xFF, 0, 0xFF];
+    if texel == EXPECTED {
+        println!("\t{}", "PASS".color(Color::Green));
+    } else {
+        println!("\t{} {:?}", "FAIL".color(Color::Red), texel);
+    }
+}
 
 fn main() {
+    use glow::HasContext;
+
     #[cfg(target_os = "macos")]
     {
         use core_foundation::{self as cf, base::TCFType};
@@ -12,120 +136,47 @@ fn main() {
         );
     }
 
-    let (gl, mut events_loop, render_loop) = {
-        use glutin::GlContext;
-        let events_loop = glutin::EventsLoop::new();
-        let window_builder = glutin::WindowBuilder::new()
-            .with_title("GL Swizzle Test")
-            .with_dimensions(glutin::dpi::LogicalSize::new(1024.0, 768.0));
-        let context_builder = glutin::ContextBuilder::new().with_vsync(true);
-        let window =
-            glutin::GlWindow::new(window_builder, context_builder, &events_loop).unwrap();
-        unsafe {
-            window.make_current().unwrap();
-        }
-        let context = glow::native::Context::from_loader_function(|s| {
-            window.get_proc_address(s) as *const _
-        });
-        let render_loop =
-            glow::native::RenderLoop::<glutin::GlWindow>::from_glutin_window(window);
-        (context, events_loop, render_loop)
+    let connection = surfman::Connection::new().unwrap();
+    let adapter = connection.create_adapter().unwrap();
+    let mut device = connection.create_device(&adapter).unwrap();
+    let context_attributes = surfman::ContextAttributes {
+        version: surfman::GLVersion::new(3, 3),
+        flags: surfman::ContextAttributeFlags::empty(),
     };
+    let context_descriptor = device
+        .create_context_descriptor(&context_attributes)
+        .unwrap();
+    let mut context = device.create_context(&context_descriptor).unwrap();
+    let gl = glow::Context::from_loader_function(|s| device.get_proc_address(&context, s));
+    device.make_context_current(&context).unwrap();
 
-    let renderer = unsafe {
-         gl.get_parameter_string(glow::RENDERER)
-    };
+    let renderer = unsafe { gl.get_parameter_string(glow::RENDERER) };
     let extensions = unsafe {
         let num = gl.get_parameter_i32(glow::NUM_EXTENSIONS);
-        (0 .. num)
-            .map(|i| {
-                gl.get_parameter_indexed_string(glow::EXTENSIONS, i as u32)
-            })
-            .filter(|ext| ext.contains("swizzle"))
+        (0..num)
+            .map(|i| gl.get_parameter_indexed_string(glow::EXTENSIONS, i as u32))
             .collect::<Vec<_>>()
     };
-    println!("Renderer: {}", renderer);
-    println!("Swizzle: {:?}", extensions);
+    let has_khr_debug = extensions.iter().any(|ext| ext.as_str() == "GL_KHR_debug");
+    println!("Init with renderer: {}", renderer.color(Color::Violet));
 
-    unsafe {
-        let size = 512;
-        let texture = gl.create_texture().unwrap();
-        gl.bind_texture(glow::TEXTURE_2D_ARRAY, Some(texture));
-        gl.tex_storage_3d(glow::TEXTURE_2D_ARRAY, 1, glow::RGBA8, size, size, 2);
-        gl.tex_parameter_i32(glow::TEXTURE_2D_ARRAY, glow::TEXTURE_SWIZZLE_R, glow::BLUE as i32);
-        gl.tex_parameter_i32(glow::TEXTURE_2D_ARRAY, glow::TEXTURE_SWIZZLE_G, glow::GREEN as i32);
-        gl.tex_parameter_i32(glow::TEXTURE_2D_ARRAY, glow::TEXTURE_SWIZZLE_B, glow::RED as i32);
-        gl.tex_parameter_i32(glow::TEXTURE_2D_ARRAY, glow::TEXTURE_SWIZZLE_A, glow::ALPHA as i32);
-    };
+    test_swizzle(&gl, &extensions);
 
-    unsafe {
-        let vertex_array = gl
-            .create_vertex_array()
-            .expect("Cannot create vertex array");
-        gl.bind_vertex_array(Some(vertex_array));
+    if has_khr_debug {
+        let debug_messages = unsafe { gl.get_debug_message_log(10) };
+        if !debug_messages.is_empty() {
+            println!("Debug messages:");
+            for msg in debug_messages {
+                println!("\t{:?}", msg);
+            }
+        }
+    } else {
+        let error = unsafe { gl.get_error() };
+        if error != glow::NO_ERROR {
+            println!("Last {}: {:?}", "ERROR".color(Color::Red), error);
+        }
     }
 
-    let program = unsafe {
-        let program = gl.create_program().expect("Cannot create program");
-        let vertex_shader_source = r#"#version 330 core
-            uniform sampler2DArray tex;
-            const vec2 verts[4] = vec2[4](
-                vec2(0.2f, 0.2f),
-                vec2(0.2f, 0.8f),
-                vec2(0.8f, 0.2f),
-                vec2(0.8f, 0.8f)
-            );
-            out vec4 color;
-            void main() {
-                vec2 tc = verts[gl_VertexID];
-                vec2 tex_size = vec2(textureSize(tex, 0).xy);
-                color = vec4(tex_size/512.0, 0.0, 1.0);
-                gl_Position = vec4(2.0*tc - 1.0, 0.0, 1.0);
-            }
-        "#;
-        let fragment_shader_source = r#"#version 330 core
-            precision mediump float;
-            in vec4 color;
-            out vec4 o_Color;
-            void main() {
-                o_Color = color;
-            }
-        "#;
-        let shader_sources = [
-            (glow::VERTEX_SHADER, vertex_shader_source),
-            (glow::FRAGMENT_SHADER, fragment_shader_source),
-        ];
-        for (shader_type, shader_source) in shader_sources.iter() {
-            let shader = gl
-                .create_shader(*shader_type)
-                .expect("Cannot create shader");
-            gl.shader_source(shader, shader_source);
-            gl.compile_shader(shader);
-            if !gl.get_shader_compile_status(shader) {
-                panic!(gl.get_shader_info_log(shader));
-            }
-            gl.attach_shader(program, shader);
-        }
-        gl.link_program(program);
-        if !gl.get_program_link_status(program) {
-            panic!(gl.get_program_info_log(program));
-        }
-        program
-    };
-
-    render_loop.run(move |running: &mut bool| {
-        events_loop.poll_events(|event| {
-            if let glutin::Event::WindowEvent { event, .. } = event {
-                if let glutin::WindowEvent::CloseRequested = event {
-                    *running = false;
-                }
-            }
-        });
-
-        unsafe {
-            gl.clear(glow::COLOR_BUFFER_BIT);
-            gl.use_program(Some(program));
-            gl.draw_arrays(glow::TRIANGLE_STRIP, 0, 4);
-        }
-    });
+    device.destroy_context(&mut context).unwrap();
+    println!("Done");
 }
